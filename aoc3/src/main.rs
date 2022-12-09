a632aa6f384ac469403eba7f88d13d3900430bc3
Viktor WSL <viktorkhotimchenko@gmail.com>
2022-12-09 00:54:17 -0500

    aoc3 pt2 GNOME RESET!

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -11,44 +11,50 @@ fn main() {
     let reader = BufReader::new(file);
 
     let mut prio = 0;
+    let mut group = 0;
+
+    let mut items: HashMap<char, u8> = HashMap::new();
 
     for gnome in reader.lines() {
-        let mut rucksack = gnome.expect("contains items");
-        let right_compartment = rucksack.split_off(rucksack.len() / 2);
+        if group == 3 {
+            // println!("GNOME RESET!");
+            items = HashMap::new();
+            group = 0;
+        }
+        group += 1;
 
-        let mut items: HashMap<char, u8> = HashMap::new();
+        let rucksack = gnome.expect("contains items");
 
         for item in rucksack.chars() {
             match items.get(&item) {
                 Some(&count) => {
-                    // println!("{:?}{:?}", item, count);
-                    items.insert(item, count + 1);
+                    // println!("{:?}{:?},{:?}", item, count, group);
+                    let next_count = count + 1;
+                    if group == next_count {
+                        items.insert(item, next_count);
+
+                        if next_count == 3 {
+                            let ascii = item as u32;
+                            let rity = if item.is_lowercase() {
+                                ascii - 96
+                            } else {
+                                ascii - 64 + 26
+                            };
+                            prio += rity;
+                            // println!("got em! {:?} ascii:{:?} prio:{:?}", item, ascii, rity);
+                            break;
+                        }
+                    }
                 }
                 None => {
-                    items.insert(item, 1);
+                    if group == 1 {
+                        items.insert(item, 1);
+                    }
                 }
             }
         }
 
-        for item in right_compartment.chars() {
-            match items.get(&item) {
-                Some(_) => {
-                    let ascii = item as u32;
-                    let rity = if item.is_lowercase() {
-                        ascii - 96
-                    } else {
-                        ascii - 64 + 26
-                        // ascii - 38 ;)?
-                    };
-                    prio += rity;
-                    // println!("got em! {:?} = {:?}", item, ascii);
-                    break;
-                }
-                None => (),
-            }
-        }
-
-        println!("{:?}, {:?} {:#?}", rucksack, right_compartment, items);
+        // println!("{:?}, {:?} {:#?}", rucksack, prio, items);
     }
     println!("{:?}", prio);
 }
