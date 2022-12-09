b021a4aa18714fc83d61e17f31359165cbed2ee2
Viktor WSL <viktorkhotimchenko@gmail.com>
2022-12-08 23:33:46 -0500

    aoc3 p1

--- aoc3/src/main.rs
+++ aoc3/src/main.rs
@@ -1,4 +1,5 @@
 use std::{
+    collections::HashMap,
     fs::File,
     io::{BufRead, BufReader},
 };
@@ -9,12 +10,45 @@ fn main() {
 
     let reader = BufReader::new(file);
 
-    for rucksack in reader.lines() {
-        let compartments = rucksack.expect("contains items");
-        for item in compartments.chars() {
-            print!("{:?}", item);
+    let mut prio = 0;
+
+    for gnome in reader.lines() {
+        let mut rucksack = gnome.expect("contains items");
+        let right_compartment = rucksack.split_off(rucksack.len() / 2);
+
+        let mut items: HashMap<char, u8> = HashMap::new();
+
+        for item in rucksack.chars() {
+            match items.get(&item) {
+                Some(&count) => {
+                    // println!("{:?}{:?}", item, count);
+                    items.insert(item, count + 1);
+                }
+                None => {
+                    items.insert(item, 1);
+                }
+            }
         }
 
-        println!("{:?}", compartments);
+        for item in right_compartment.chars() {
+            match items.get(&item) {
+                Some(_) => {
+                    let ascii = item as u32;
+                    let rity = if item.is_lowercase() {
+                        ascii - 96
+                    } else {
+                        ascii - 64 + 26
+                        // ascii - 38 ;)?
+                    };
+                    prio += rity;
+                    // println!("got em! {:?} = {:?}", item, ascii);
+                    break;
+                }
+                None => (),
+            }
+        }
+
+        println!("{:?}, {:?} {:#?}", rucksack, right_compartment, items);
     }
+    println!("{:?}", prio);
 }
